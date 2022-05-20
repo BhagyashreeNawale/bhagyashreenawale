700d5432ef4a7b02c321f2c72a08863b46e340bb
bhagyashree.nawale <bhagyashree.nawale@AZ-VDI-D1-240>
2022-05-20 06:15:31 -0700

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+
+namespace Assignment1
+{
+   
+
+    class Program
+    {
+        public void RaisedTo(double x , double y)
+        {
+
+            Console.WriteLine(  Math.Pow(x, y) );
+            
+        }
+        public void Fact(double x)
+        {
+            double z = 1;
+            for(int i =1;i<=x; i++)
+            {
+                 z = x *(double)i;
+            }
+            Console.WriteLine(z);
+        }
+        static void Main(string[] args)
+        {
+            Program a = new Program();
+            a.RaisedTo(2.0, 3.0);
+            a.Fact(2);
+           
+
+        }
+    }
+}
